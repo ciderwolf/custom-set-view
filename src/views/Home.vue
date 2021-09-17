be507ab3371e2d95c60238e8abbb5822d4d92630
bsiderowf <bsiderowf@gmail.com>
2021-09-16 23:05:50 -0500

    Link to discord

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,12 @@
       <input type="text" placeholder="Search for cards..." v-model="searchText" id="search-field">
     </form>
     <i class="nav-hint">or press Enter to see all cards</i>
-    <router-link to="/deckbuilder">Deckbuilder</router-link>
+    <div class="navigation">
+      <router-link to="/deckbuilder">Deckbuilder</router-link>
+      <a href="https://discord.gg/RyhCxGeyKR" id="discord-link" target="_blank">
+        <img src='@/assets/discord-logo.svg'>Join our Discord
+      </a>
+    </div>
     <p class="version">Innistrad Exhumed 4.6</p>
   </div>
 </template>
@@ -65,14 +70,43 @@ form {
   margin-top: 10vh;
 }
 
-a {
-  color: blue;
-  text-decoration: none;
-  margin-top: 20px;
+#discord-link {
+  display: flex;
+  align-items: center;
 }
 
-a:hover {
-  text-decoration: underline;
+#discord-link img {
+  height: 16px;
+  margin-right: 4px;
+}
+
+.navigation {
+  display: flex;
+  width: 300px;
+  margin-top: 20px;
+  justify-content: space-around;
+}
+
+.navigation a {
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 0 10px;
+  height: 28px;
+  font-size: 14px;
+  font-weight: 500;
+  color: #484848;
+  text-decoration: none;
+  border: 1px solid rgba(41, 41, 41, 0.5);
+  border-radius: 3px;
+  background-color: #f8f8f8;
+  transition: border-color, background-color 0.3s linear;
+}
+
+.navigation a:hover {
+  background-color: white;
+  border-color: rgba(41, 41, 41, 0.75);
 }
 
 .version {
